c150ac1e8a8ac4e797422ae43d0030c1d86a3358
Isaac Elbaz <ielbaz@openaristos.io>
2020-05-03 18:30:58 -0400

    Point CI/CD to GitHub

--- ci/concourse/build.sh
+++ ci/concourse/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git clone https://git@bitbucket.org/openaristos/openaristos-java-sdk.git
+git clone https://github.com/openaristos/openaristos-java-sdk.git
 cd openaristos-java-sdk
 mvn install
 
